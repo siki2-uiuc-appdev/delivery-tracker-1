b36de2a74a95fd03263ed0d99dc4bb2fca994f0c
siki2-uiuc <siki2@illinois.edu>
2022-11-10 16:02:32 +0000

    Delivery RCAV migrated

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: deliveries
+#
+#  id           :integer          not null, primary key
+#  arrival_date :datetime
+#  description  :string
+#  details      :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  owner_id     :integer
+#
 class Delivery < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_10_155713) do
+ActiveRecord::Schema.define(version: 2022_11_10_160048) do
+
+  create_table "deliveries", force: :cascade do |t|
+    t.string "description"
+    t.datetime "arrival_date"
+    t.text "details"
+    t.integer "owner_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
